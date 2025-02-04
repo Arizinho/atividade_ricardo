70c984d6eb8dca463d895b11be4204d43d9c55fd
Ariagildo L <ariagildo01@gmail.com>
2025-02-03 23:19:53 -0300

    configura pinos, temporizador e função callback do temporizador

--- semaforo.c
+++ semaforo.c
@@ -1,11 +1,52 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/timer.h"
 
+#define RED_LED 13
+#define BLUE_LED 12
+#define GREEN_LED 11
 
+bool repeating_timer_callback(struct repeating_timer *t){
+    static uint8_t count = 0;
+    switch (count)
+    {
+    case 0:
+        gpio_put(RED_LED, 1);
+        gpio_put(BLUE_LED, 1);
+        gpio_put(GREEN_LED, 1);
+        break;
+
+    case 1:
+        gpio_put(RED_LED, 1);
+        gpio_put(BLUE_LED, 0);
+        gpio_put(GREEN_LED, 1);
+        break;
+    
+    default:
+        gpio_put(RED_LED, 0);
+        gpio_put(BLUE_LED, 0);
+        gpio_put(GREEN_LED, 1);
+        break;
+    }
+    count = (count + 1) % 3;
+    return true;
+}
 
 int main()
 {
     stdio_init_all();
+
+    gpio_init(RED_LED);
+    gpio_init(BLUE_LED);
+    gpio_init(GREEN_LED);
+
+    gpio_set_dir(RED_LED, GPIO_OUT);
+    gpio_set_dir(BLUE_LED, GPIO_OUT);
+    gpio_set_dir(GREEN_LED, GPIO_OUT);
+
+    struct repeating_timer timer;
+
+    add_repeating_timer_ms(3000, repeating_timer_callback, NULL, &timer); 
 
     while (true) {
         printf("Hello, world!\n");
